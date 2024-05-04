cd462f4fba00b4d418fd5f7c78ac77529a80dd0d
Nisha Bukke <sononenisha827@gmail.com>
2024-05-04 12:40:20 +0530

    php code added

--- validate.php
+++ validate.php
@@ -1,13 +1,14 @@
 <?php
 //get data from form  
 
-$name = $_POST['fname'];
+$first_name = $_POST['fname'];
 $email= $_POST['email'];
-$message= $_POST['lname'];
+$last_name= $_POST['lname'];
+$message= $_POST['message'];
 $to = "sononenisha@gmail.com";
-$subject = "Mail From website";
-$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
-$headers = "From: noreply@yoursite.com";
+$subject = "Mail from the website";
+$txt ="First Name = ". $fname . "\r\n  Last Name = " . $lname . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+$headers = ['From' => $email, 'Reply-To' => $email, 'Content-type' => 'text/html; charset=utf-8'];
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
 }
